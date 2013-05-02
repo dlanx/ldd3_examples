4721cf48f7a7fa13816d715c5af2efa7a4a90190
Lan Yixun (dlan) <dennis.yxun@gmail.com>
2013-05-02 16:18:49 +0800

    sculld: change work_struct
    65f27f38446e1976cc98fd3004b110fedcddd189
    
    Signed-off-by:  Lan Yixun (dlan) <dennis.yxun@gmail.com>

--- sculld/main.c
+++ sculld/main.c
@@ -409,15 +409,17 @@ loff_t sculld_llseek (struct file *filp, loff_t off, int whence)
 struct async_work {
 	struct kiocb *iocb;
 	int result;
-	struct work_struct work;
+	struct delayed_work timeout_work;
 };
 
 /*
  * "Complete" an asynchronous operation.
  */
-static void sculld_do_deferred_op(void *p)
+static void sculld_do_deferred_op(struct work_struct *work)
 {
-	struct async_work *stuff = (struct async_work *) p;
+	struct async_work *stuff =
+		container_of(work, struct async_work, timeout_work.work);
+
 	aio_complete(stuff->iocb, stuff->result, 0);
 	kfree(stuff);
 }
@@ -445,8 +447,8 @@ static int sculld_defer_op(int write, struct kiocb *iocb, char __user *buf,
 		return result; /* No memory, just complete now */
 	stuff->iocb = iocb;
 	stuff->result = result;
-	INIT_WORK(&stuff->work, sculld_do_deferred_op, stuff);
-	schedule_delayed_work(&stuff->work, HZ/100);
+	INIT_DELAYED_WORK(&stuff->timeout_work, sculld_do_deferred_op);
+	schedule_delayed_work(&stuff->timeout_work, HZ/100);
 	return -EIOCBQUEUED;
 }
 
